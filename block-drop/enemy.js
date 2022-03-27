2e9a72debc9ad08270a6db284de9643d8097462b
Epoza <jacobpittsley2@gmail.com>
2022-03-27 14:20:42 -0400

    Added files

---
+++ block-drop/enemy.js
@@ -0,0 +1,61 @@
+export default class Enemy{
+
+    constructor(x, y, color, health, speedY){
+        //this.x = x;
+        this.x = Math.floor(Math.random() * x) + 1;
+        this.y = y;
+        this.color = color;
+        this.health = Math.floor(Math.random() * health) + 1;
+        this.speedY = speedY
+        this.width = 50;
+        this.height = 50;
+    }
+    draw(ctx){
+        ctx.fillStyle = this.color;
+        if(this.health > 1){
+            ctx.strokeStyle = 'white'
+        }else {
+            ctx.strokeStyle = this.color
+        }
+
+        // function createEnemy() {
+        //     new Enemy(500, 20, "green", 5, 1)
+        // }
+        
+        // var enemyInterval = setInterval(createEnemy, 1000);
+        // enemyInterval
+
+        if (this.y <= 600) {
+            this.y += this.speedY
+        }else{
+            this.speedY = 0
+        }
+
+        if(this.speedY <= 0) {
+            document.getElementById("gameover-btn").style.display = "block";
+        }
+
+        
+
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+
+        //Draw Text
+        ctx.fillStyle = 'black';
+        ctx.font = '25px Arial';
+        ctx.fillText(this.health, this.x +this.width/3.5, this.y+this.height/1.5)
+    }
+
+    takeDamage(damage){
+        this.health -= damage
+    }
+    
+}
+
+//code for enemy touching bottom
+// if(this.y <= 0) {
+//     gameover
+// }
+
+// if enemy <= 100 or whatever
+//set all enemies to hidden or stop the random enemy function by making variable game over or smthn
---
+++ block-drop/script.js
@@ -0,0 +1,62 @@
+import Player from "./player.js";
+import Enemy from "./enemy.js";
+import BulletController from "./bulletController.js";
+
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+let points = document.getElementById('points');
+let frameCount = 0
+
+canvas.width = 550
+canvas.height = 600
+
+
+
+const bulletController = new BulletController(canvas);
+// sets player location
+const player = new Player(canvas.width / 2.2, canvas.height / 1.3, bulletController)
+
+const enemies = [
+    new Enemy(500, 20, "green", 5, 1),
+    new Enemy(500, 20, "red", 5, 1),
+    new Enemy(500, 20, "gold", 5, 1),
+    new Enemy(500, 20, "green", 5, 1),
+    new Enemy(500, 20, "gold", 10, 1),
+    new Enemy(500, 100, "green", 10, 1),
+    new Enemy(500, 100, "red", 10, 1),
+    new Enemy(500, 100, "gold", 20, 1),
+    new Enemy(500, 100, "green", 20, 1),
+    new Enemy(500, 100, "gold", 20, 1),
+];
+
+
+function gameLoop() {
+        setCommonStyle();
+    frameCount++;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    bulletController.draw(ctx);
+    player.draw(ctx);
+    enemies.forEach((enemy) =>{
+        if(bulletController.collideWith(enemy)){
+            if(enemy.health <= 0) {
+                const index = enemies.indexOf(enemy);
+                enemies.splice(index, 1);
+            }
+        }else{
+        enemy.draw(ctx);
+        }
+    });
+}
+
+function setCommonStyle() {
+    ctx.shadowColor = '#d53';
+    ctx.shadowBlue = 20;
+    ctx.lineJoin = 'Bevel';
+    ctx.lineWidth = 5
+}
+var intervalId = setInterval(gameLoop, 1000/60)
+intervalId
+
+
+//make gameover
