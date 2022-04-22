dda37f561cb6f31159cb44a3da0e2098cf9d54b0
Epoza <jacobpittsley2@gmail.com>
2022-04-22 13:57:32 -0400

    changed file

--- space-battle/script.js
+++ space-battle/script.js
@@ -1,6 +1,7 @@
-import EnemyController from "./EnemyController.js";
-import Player from "./Player.js";
+import Player1 from "./Player.js";
+import Player2 from "./Player2.js";
 import bulletController from "./BulletController.js";
+import bulletController2 from "./BulletController2.js";
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
@@ -8,54 +9,71 @@ const ctx = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
 
+let player1Points = 0;
+let player2Points = 0;
+
 const background = new Image();
 background.src = 'images/space.png';
 
-const playerBulletController = new bulletController(canvas, 10, "red", true)
-const enemyBulletcontroller = new bulletController(canvas, 4, 'white', true)
-const enemyController = new EnemyController(canvas, enemyBulletcontroller, playerBulletController);
-const player = new Player(canvas, 3, playerBulletController);
+const player1BulletController = new bulletController(canvas, 5, "red", true)
+const player2BulletController = new bulletController2(canvas, 5, "blue", true)
+const player1 = new Player1(canvas, 3, player1BulletController, player2BulletController);
+const player2 = new Player2(canvas, 3, player2BulletController);
 
 let isGameOver = false;
-let didWin = false
+let whoWon = false;
 
 function game(){
-    checkGameOver();
+    checkForPoint();
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     displayGameOver();
     if(!isGameOver){
-        enemyController.draw(ctx);
-        player.draw(ctx);
-        playerBulletController.draw(ctx);
-        enemyBulletcontroller.draw(ctx)
+        player1.draw(ctx);
+        player2.draw(ctx);
+        player1BulletController.draw(ctx);
+        player2BulletController.draw(ctx);
+        ctx.fillStyle = "white"
+        ctx.font = "20px Arial"
+        ctx.fillText("Points: " + player1Points, canvas.width - 590, canvas.height - 10)
+        ctx.fillText("Points: " + player2Points, canvas.width - 590, canvas.height - 575)
     }
 }
 
-function displayGameOver() {
-    if (isGameOver) {
-        let text = didWin ? "You Win" : "Game Over";
-        let textOffset = didWin ? 3.5 : 5;
-    
-        ctx.fillStyle = "white";
-        ctx.font = "70px Arial";
-        ctx.fillText(text, canvas.width / textOffset, canvas.height / 2);
-    }
-}
-
-function checkGameOver() {
-    if(isGameOver){
+function checkForPoint(){
+    if(isGameOver) {
         return;
     }
 
-    if(enemyBulletcontroller.collideWith(player)){
-        isGameOver = true;
+    if(player1BulletController.collideWith(player2)){
+        player1Points++;
+        if (player1Points === 5){
+            isGameOver = true;
+        }
     }
-    if(enemyController.collideWith(player)) {
-        isGameOver = true
-    }
-    if(enemyController.enemyRows.length === 0){
-        didWin = true;
-        isGameOver = true
+    if(player2BulletController.collideWith(player1)){
+        player2Points++;
+        if (player2Points === 5){
+            whoWon = true;
+            isGameOver = true;
+        }
     }
 }
+
+function displayGameOver(){
+    if(isGameOver){
+        let mainText = whoWon ? "Player 2 Wins!" : "Player 1 Wins!";
+        let mainTextOffset = 8;
+        let winColor =  whoWon ? "blue" : "red"
+
+        ctx.fillStyle = winColor;
+        ctx.font = "70px Arial";
+        ctx.fillText(mainText, canvas.width / mainTextOffset, canvas.height / 2);
+        ctx.fillStyle = "white"
+        ctx.font = "25px Arial";
+        ctx.fillText("Player 1 Points: " + player1Points, canvas.width / 2.8, canvas.height / 1.4)
+        ctx.fillText("Player 2 Points: " + player2Points, canvas.width / 2.8, canvas.height / 4.5)
+    }
+}
+
+
 setInterval(game, 1000/60);
